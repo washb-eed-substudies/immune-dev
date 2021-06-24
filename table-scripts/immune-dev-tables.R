38b33abb1d202779c2883f76d7581ac53d580c1b
Sophia Tan <sophiatan@berkeley.edu>
2021-06-24 12:40:24 -0700

    added enrollment table

--- table-scripts/immune-dev-tables.R
+++ table-scripts/immune-dev-tables.R
@@ -4,7 +4,7 @@ source(here::here("0-config.R"))
 source(here::here("table-functions.R"))
 
 # load enrollment characteristics and results
-# d <- read.csv(paste0(dropboxDir, "Data/Cleaned/Audrie/bangladesh-dm-ee-stress-growth-covariates-stresslab-anthro.csv"))
+d <- readRDS(paste0(dropboxDir,"Data/Cleaned/Audrie/bangladesh-immune-development-analysis-dataset.rds"))
 H1 <- readRDS(here('results/unadjusted/H1_res.RDS'))
 H2 <- readRDS(here('results/unadjusted/H2_res.RDS'))
 H3 <- readRDS(here('results/unadjusted/H3_res.RDS'))
@@ -23,53 +23,82 @@ adj <- rbind(H1adj, H2adj, H3adj, H4adj, indadj)
 
 #### MAIN TABLES ####
 # #### Table 1 ####
-# # Characteristics of participants
-# nperc <- function(vector){
-#   n <- sum(vector==1, na.rm=T)
-#   perc <- round(n/sum(!is.na(vector))*100)
-#   paste(n, " (", perc, "%)", sep="")
-# }
-# 
-# mediqr <- function(vector){
-#   quantiles <- round(quantile(vector, na.rm=T), 2)
-#   paste(quantiles[3], " (", quantiles[2], ", ", quantiles[4], ")", sep="")
-# }
-# 
-# n_med_col <- c(nperc(d$sex), mediqr(d$t2_f2_8ip), mediqr(d$t2_f2_23d), mediqr(d$t2_f2_VI), mediqr(d$t2_f2_12i),
-#                mediqr(d$t3_cort_slope), mediqr(d$t3_residual_cort), mediqr(d$t3_saa_slope), mediqr(d$t3_residual_saa),
-#                mediqr(d$t3_map), mediqr(d$t3_hr_mean), mediqr(d$t3_gcr_mean), mediqr(d$t3_gcr_cpg12),
-#                mediqr(d$laz_t2), mediqr(d$waz_t2), mediqr(d$whz_t2), mediqr(d$hcz_t2),
-#                mediqr(d$laz_t3), mediqr(d$waz_t3), mediqr(d$whz_t3), mediqr(d$hcz_t3),
-#                nperc(d$diar7d_t2), nperc(d$diar7d_t3), mediqr(d$momage), mediqr(d$momheight), 
-#                mediqr(d$momeduy), mediqr(d$cesd_sum_t2), mediqr(d$cesd_sum_ee_t3), mediqr(d$pss_sum_mom_t3), 
-#                nperc(d$life_viol_any_t3))
-# 
-# tbl1 <- data.table("C1" = c("Child","","","","","","","","","","","","","","","","","","","","","","","Mother","","","","","",""),
-#                    "C2" = c("", "Urinary F2-isoprostanes (Year 1)","","","", "Salivary cortisol reactivity (Year 2)","", "sAA reactivity (Year 2)","",
-#                            "SAM biomarkers (Year 2)","", "Glucocorticoid receptor","", "Anthropometry (14 months, Year 1)","","","",
-#                            "Anthropometry (28 months, Year 2)","","","", "Diarrhea (14 months, Year 1)", "Diarrhea (28 months, Year 2)","",
-#                            "Anthropometry at enrollment", "Education", "Depression at Year 1", "Depression at Year 2", "Perceived stress at Year 2", 
-#                            "Intimate partner violence"),
-#                    "C3" = c("Female", "iPF(2a)-III", "2,3-dinor-iPF(2a)-III", "iPF(2a-VI", "8,12-iso-iPF(2a)-VI", 
-#                            "Change in slope between pre- and post-stressor cortisol", "Cortisol residualized gain score", 
-#                            "Change in slope between pre- and post-stressor sAA change", "sAA residualized gain score",
-#                            "Mean arterial pressure", "Resting heart rate", "NR3C1 exon 1F promoter methylation", "NGFI-A transcription factor binding site methylation",
-#                            "Length-for-age Z score", "Weight-for-age Z score", "Weight-for-length Z score", "Head circumference-for-age Z score",
-#                            "Length-for-age Z score", "Weight-for-age Z score", "Weight-for-length Z score", "Head circumference-for-age Z score",
-#                            "Caregiver-reported 7-day recall", "Caregiver-reported 7-day recall", "Age (years)", "Height (cm)", "Schooling completed (years)",
-#                            "CES-D score", "CES-D score", "Perceived Stress Scale score", "Any lifetime exposure"),
-#                    "C4" = n_med_col)
-# 
-# tbl1flex <- flextable(tbl1, col_keys=names(tbl1))
-# tbl1flex <- set_header_labels(tbl1flex,
-#                         values = list("C1" = "", "C2" = "", "C3" = "", "C4" = "n (%) or median (IQR)"))
-# tbl1flex <- hline_top(tbl1flex, part="header", border=fp_border(color="black", width = 1))
-# tbl1flex <- hline_bottom(tbl1flex, part="all", border=fp_border(color="black", width = 1))
-# tbl1flex <- autofit(tbl1flex, part = "all")
-# tbl1flex <- align(tbl1flex, j = c(1, 2, 3), align = "left", part="all")
-# tbl1flex <- align(tbl1flex, j = 4, align = "center", part="all")
-# tbl1flex <- fit_to_width(tbl1flex, max_width=8)
-# names(tbl1)<- c("","","","n (%) or median (IQR)")
+filtering <- function(row){
+  any(!is.na(row))
+}
+
+exp_t2 <- c(grep("t2_ln", names(d), value=TRUE), "sumscore_t2_Z")
+out_t2 <- c("sum_who", "z_cdi_und_t2", "z_cdi_say_t2")
+exp_t3 <- c(grep("t3_ln", names(d), value=TRUE), "sumscore_t3_Z")
+out_t3 <- c("z_comm_easq", "z_motor_easq", "z_personal_easq", "z_combined_easq", 
+            "z_cdi_say_t3", "z_cdi_und_t3")
+t2 <- d[apply(select(d, all_of(exp_t2)), 1, filtering),] # only has rows where we have exposure data at t2
+t2 <- t2[apply(select(t2, all_of(c(out_t2, out_t3))), 1, filtering),] # only has rows where we have both some exposure data and some outcome data
+t3 <- d[apply(select(d, all_of(exp_t3)), 1, filtering),] # only has rows where we have exposure data at t3
+t3 <- t3[apply(select(t3, all_of(out_t3)), 1, filtering),] # only has rows where we have both some exposure data and some outcome data
+d <- t2 %>% full_join(t3, by=names(d)) # has all children included in this analysis
+
+characteristics <- function(d, child_char = NULL, child_char_names = NULL, mom_char = NULL, mom_char_names = NULL) {
+  nperc <- function(vector){
+    n <- sum(vector==1, na.rm=T)
+    perc <- round(n/sum(!is.na(vector))*100)
+    paste(n, " (", perc, "%)", sep="")
+  }
+  
+  mediqr <- function(vector){
+    quantiles <- round(quantile(vector, na.rm=T), 2)
+    paste(quantiles[3], " (", quantiles[2], ", ", quantiles[4], ")", sep="")
+  }
+  
+  child <- c('sex',child_char,'laz_t1','waz_t1','whz_t1','hcz_t1',
+             'laz_t2','waz_t2','whz_t2','hcz_t2',
+             'laz_t3','waz_t3','whz_t3','hcz_t3','diar7d_t2','diar7d_t3')
+  
+  mom <- c('momage', 'momheight', 'momeduy', mom_char, 'cesd_sum_t2', 'cesd_sum_ee_t3', 'pss_sum_mom_t3', 'life_viol_any_t3')
+  
+  n_med_col <- NULL
+  for (var in c(child, mom)) {
+    if (var %in% c('sex', 'diar7d_t2', 'diar7d_t3', 'life_viol_any_t3') | is.factor(d[[var]])) {
+      if (var == 'sex') {
+        n <- sum(d$sex=='female', na.rm=T)
+        perc <- round(n/sum(!is.na(d$sex))*100)
+        n_med_col <- c(n_med_col, paste(n, " (", perc, "%)", sep=""))
+      }else {
+        d[[var]] <- na_if(d[[var]], "Missing")
+        n_med_col <- c(n_med_col, nperc(d[[var]]))
+      }
+    }else {
+      n_med_col <- c(n_med_col, mediqr(d[[var]]))
+    }
+  }
+  
+  tbl1 <- data.table("C1" = c("Child", rep("", length(child)-1),"Mother", rep("",length(mom)-1)),
+                     "C2" = c("", rep("", length(child_char)), "Anthropometry (3 months, Year 1)","","","",
+                              "Anthropometry (14 months, Year 1)","","","",
+                              "Anthropometry (28 months, Year 2)","","","", "Diarrhea (14 months, Year 1)", "Diarrhea (28 months, Year 2)","",
+                              "Anthropometry at enrollment", "Education", rep("", length(mom_char)), "Depression at Year 1", "Depression at Year 2", "Perceived stress at Year 2", 
+                              "Intimate partner violence"),
+                     "C3" = c("Female", child_char_names,
+                              "Length-for-age Z score", "Weight-for-age Z score", "Weight-for-length Z score", "Head circumference-for-age Z score",
+                              "Length-for-age Z score", "Weight-for-age Z score", "Weight-for-length Z score", "Head circumference-for-age Z score",
+                              "Length-for-age Z score", "Weight-for-age Z score", "Weight-for-length Z score", "Head circumference-for-age Z score",
+                              "Caregiver-reported 7-day recall", "Caregiver-reported 7-day recall", "Age (years)", "Height (cm)", "Schooling completed (years)",
+                              mom_char_names, "CES-D score", "CES-D score", "Perceived Stress Scale score", "Any lifetime exposure"),
+                     "C4" = n_med_col)
+  
+  tbl1flex <- flextable(tbl1, col_keys=names(tbl1))
+  tbl1flex <- set_header_labels(tbl1flex,
+                                values = list("C1" = "", "C2" = "", "C3" = "", "C4" = "n (%) or median (IQR)"))
+  tbl1flex <- hline_top(tbl1flex, part="header", border=fp_border(color="black", width = 1))
+  tbl1flex <- hline_bottom(tbl1flex, part="all", border=fp_border(color="black", width = 1))
+  tbl1flex <- autofit(tbl1flex, part = "all")
+  tbl1flex <- align(tbl1flex, j = c(1, 2, 3), align = "left", part="all")
+  tbl1flex <- align(tbl1flex, j = 4, align = "center", part="all")
+  tbl1flex <- fit_to_width(tbl1flex, max_width=8)
+  tbl1flex
+}
+
+enroll <- characteristics(d)
 
 
 #### Table 2 ####
@@ -221,15 +250,15 @@ tbl9flexsupp <- hr_tbl_flex("IGF-1", expo_var, out_var, exposure, outcome, HR, H
 
 #### SAVE TABLES ####
 
-# write.csv(tbl1, file=here("tables/main/stress-growth-table1.csv"))
-write.csv(tbl2, here('tables/main/immune-dev-table1.csv'))
-write.csv(tbl3, here('tables/main/immune-dev-table2.csv'))
-write.csv(tbl4, here('tables/main/immune-dev-table3.csv'))
-write.csv(tbl5, here('tables/main/immune-dev-table4.csv'))
-write.csv(tbl6, here('tables/main/immune-dev-table5.csv'))
-write.csv(tbl7, here('tables/main/immune-dev-table6.csv'))
-write.csv(tbl8, here('tables/main/immune-dev-table7.csv'))
-write.csv(tbl9, here('tables/main/immune-dev-table8.csv'))
+#write.csv(tbl1, file=here('tables/main/immune-dev-table1.csv'))
+write.csv(tbl2, here('tables/main/immune-dev-table2.csv'))
+write.csv(tbl3, here('tables/main/immune-dev-table3.csv'))
+write.csv(tbl4, here('tables/main/immune-dev-table4.csv'))
+write.csv(tbl5, here('tables/main/immune-dev-table5.csv'))
+write.csv(tbl6, here('tables/main/immune-dev-table6.csv'))
+write.csv(tbl7, here('tables/main/immune-dev-table7.csv'))
+write.csv(tbl8, here('tables/main/immune-dev-table8.csv'))
+write.csv(tbl9, here('tables/main/immune-dev-table9.csv'))
 
 write.csv(tbl2supp, here('tables/supplementary/immune-dev-table1.csv'))
 write.csv(tbl3supp, here('tables/supplementary/immune-dev-table2.csv'))
@@ -241,15 +270,16 @@ write.csv(tbl8supp, here('tables/supplementary/immune-dev-table7.csv'))
 write.csv(tbl9supp, here('tables/supplementary/immune-dev-table8.csv'))
 
 
-save_as_docx("Table 1: Association between Immune Status and Child Development Outcomes at Year 1" = tbl2flex, 
-             "Table 2: Hazard Ratio for Motor Milestone Attainment for Immune Status at Year 1" = tbl3flex, 
-             "Table 3: Association between Immune Status and EASQ Scores at Year 2" = tbl4flex, 
-             "Table 4: Association between Immune Status and CDI Scores at Year 2" = tbl5flex, 
-             "Table 5: Association between Immune Status at Year 1 and EASQ Scores at Year 2" = tbl6flex, 
-             "Table 6: Association between Immune Status at Year 1 and CDI Scores at Year 2" = tbl7flex, 
-             "Table 7: Association between IGF-1 and Child Development" = tbl8flex, 
-             "Table 8: Hazard Ratio for Motor Milestone Attainment for IGF-1" = tbl9flex, 
-             path='C:/Users/Sophia/Documents/WASH/WASH Immune and Child Development/immune-dev main tables v9.docx', 
+save_as_docx("Table 1: Characteristics of Participants" = enroll, 
+             "Table 2: Association between Immune Status and Child Development Outcomes at Year 1" = tbl2flex, 
+             "Table 3: Hazard Ratio for Motor Milestone Attainment for Immune Status at Year 1" = tbl3flex, 
+             "Table 4: Association between Immune Status and EASQ Scores at Year 2" = tbl4flex, 
+             "Table 5: Association between Immune Status and CDI Scores at Year 2" = tbl5flex, 
+             "Table 6: Association between Immune Status at Year 1 and EASQ Scores at Year 2" = tbl6flex, 
+             "Table 7: Association between Immune Status at Year 1 and CDI Scores at Year 2" = tbl7flex, 
+             "Table 8: Association between IGF-1 and Child Development" = tbl8flex, 
+             "Table 9: Hazard Ratio for Motor Milestone Attainment for IGF-1" = tbl9flex, 
+             path='C:/Users/Sophia/Documents/WASH/WASH Immune and Child Development/immune-dev main tables v10.docx', 
              pr_section = sect_properties)
 
 save_as_docx("Table 1: Association between Immune Status and Child Development Outcomes at Year 1" = tbl2flexsupp, 
@@ -262,11 +292,3 @@ save_as_docx("Table 1: Association between Immune Status and Child Development O
              "Table 8: Hazard Ratio for Motor Milestone Attainment for IGF-1" = tbl9flexsupp, 
              path='C:/Users/Sophia/Documents/WASH/WASH Immune and Child Development/immune-dev supplementary v9.docx',
              pr_section = sect_properties)
-
-# # test save
-# sect_properties <- prop_section(
-#   page_size = page_size(orient = "portrait", width=8.5, height=11),
-#   page_margins = page_mar(bottom=.3, top=.3, right=.3, left=.3, gutter = 0)
-# )
-# save_as_docx("tbl1" = autofit(tbl2flex, part="body"), 
-#              path='C:/Users/Sophia/Documents/WASH/WASH Immune and Child Development/temp.docx', pr_section = sect_properties)
